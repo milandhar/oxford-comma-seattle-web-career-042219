6dc0dce7ec4e278ee8ac8fcbc0671c0a9b4fdcab
Milan <milan.dhar@gmail.com>
2019-03-04 09:56:05 -0800

    method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  list = ""
+  if array.length == 1
+    list = array.join
+
+  elsif array.length == 2
+    list = array.join(" and ")
+
+  else
+    for i in 0..(array.length - 2) do
+      list << "#{array[i]}, "
+    end
+      list << "and #{array[array.length - 1]}"
+  end
+
+list
 
 end
